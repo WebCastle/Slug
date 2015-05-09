789dab31ba4165906e0e1b2a5b4b3b0329547e62
Vlad Lavro <vm.lavro@gmail.com>
2015-05-09 17:16:56 +0300

    init commit

---
+++ src/SlugServiceProvider.php
@@ -0,0 +1,32 @@
+<?php namespace Webcastle\Slug;
+
+use Illuminate\Support\ServiceProvider;
+
+use Symfony\Component\Finder\Finder;
+
+use Illuminate\Filesystem\Filesystem;
+
+class SlugServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        $this->loadAutoloader(base_path('packages'));
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        dd(1);
+	}
+
+
+}
